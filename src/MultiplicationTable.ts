d4813e5b8a86697244d77964224783f61d5ae8a6
Bingqi <BLiu25@slb.com>
2022-10-25 17:23:23 +0800

    feat: add isStartNotGreaterThanEnd function test

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,18 @@
 export class MultiplicationTable {
   public render(start: number, end: number): string{
+    const isInTheRange = IsInTheRange(start,end);
+    const isStartNotGreaterThanEnd = IsStartNotGreaterThanEnd(start,end);
+
+
     return '1*1=1'
   }
 }
+
+export function IsInTheRange(start:number,end:number):boolean{
+  return start>=1 && end<=10;
+}
+
+export function IsStartNotGreaterThanEnd(start:number,end:number):boolean{
+  return start<=end;
+}
+
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,4 +1,4 @@
-import {MultiplicationTable} from '../src/MultiplicationTable'
+import {MultiplicationTable,IsInTheRange,IsStartNotGreaterThanEnd} from '../src/MultiplicationTable'
 
 describe('Multiplication Table', () => {
   it('should render multiplication table of (1, 1)', () => {
@@ -11,6 +11,19 @@ describe('Multiplication Table', () => {
     const rendered = table.render(start, end)
 
     //then
-    expect(rendered).toBe('1*1=1')
-  })
+    expect(rendered).toBe('1*1=1');
+  });
+
+  it('is the start not larger than end (1, 1)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 1
+    const end = 1
+
+    //when
+    const isStartNotGreaterThanEnd = IsStartNotGreaterThanEnd(start, end)
+
+    //then
+    expect(isStartNotGreaterThanEnd).toBeTruthy();
+  });
 })
