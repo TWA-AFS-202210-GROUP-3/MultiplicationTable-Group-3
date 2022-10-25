996a4f30f1e1cb38900344b246f8ee893ad242c7
hdeng <hdeng10@slb.com>
2022-10-25 17:36:05 +0800

    'numvervalid'

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,16 @@
 export class MultiplicationTable {
   public render(start: number, end: number): string{
-    return '1*1=1'
+    const bool_isStartNotGreaterThanEnd=this.isStartNotGreaterThanEnd(start,end)
+    const bool_isStartandEndIntheRange=this.isStartandEndIntheRange(start,end)
+    if (bool_isStartandEndIntheRange&&bool_isStartNotGreaterThanEnd){return '1*1=1'}
+    else{return ''}
+  
+  }
+  isStartNotGreaterThanEnd(start: number, end: number):boolean{
+      return(start<=end)
+  }
+  isStartandEndIntheRange(start: number, end: number):boolean{
+    return start>=0&&start<=10&&end>=0&&end<=10
   }
 }
+
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,6 +1,19 @@
 import {MultiplicationTable} from '../src/MultiplicationTable'
 
 describe('Multiplication Table', () => {
+  it('shoudl render return null string when start larger than end (2, 1)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 1
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
+  })
+
   it('should render multiplication table of (1, 1)', () => {
     // given
     const table = new MultiplicationTable()
@@ -12,5 +25,18 @@ describe('Multiplication Table', () => {
 
     //then
     expect(rendered).toBe('1*1=1')
+  })
+
+  it('should render return null string when start not in range', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = -1
+    const end = 1
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
   })
 })
