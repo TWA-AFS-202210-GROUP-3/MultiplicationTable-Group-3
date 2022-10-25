f7115731c79fbe2648265bc0734a8a5bbb624857
Qinlan Weng <QWeng@slb.com>
2022-10-25 17:18:53 +0800

    test: add test for not valid input

--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -13,4 +13,30 @@ describe('Multiplication Table', () => {
     //then
     expect(rendered).toBe('1*1=1')
   })
+
+  it('should return empty string when start is greater than end', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 3
+    const end = 1
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
+  })
+
+  it('should return empty string when start is out of range', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 13
+    const end = 1
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
+  })
 })
