d43e47e860e8c91e183e8e2480a6bd524bb393ac
Fengjun Ma <FMa6@slb.com>
2022-10-25 17:38:39 +0800

    feat: add generate multiplication table function and add unit test

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,64 @@
 export class MultiplicationTable {
+
   public render(start: number, end: number): string{
-    return '1*1=1'
+    
+    if(!IsInputVaild(start, end))
+    {
+      return "";
+    }
+    else
+    {
+      return GenerateMultiplicationTable(start, end);
+    }
+  }  
+}
+
+function GenerateMultiplicationTable(start: number, end: number):string{
+  const distanceBetweenStartandEnd = end - start
+  var multiplicationTable = "";
+  var length;
+  for(var i=0; i<=distanceBetweenStartandEnd; i++)    ////loop for every line
+  {
+    var lineNum = start + i
+    for(var j=0; j<i+1; j++) //////loop for every column of one line
+    {
+      multiplicationTable = multiplicationTable + GenerateCell(start+j,lineNum) + '  '
+    }
+    multiplicationTable = multiplicationTable.substring(0, multiplicationTable.length-2)
+    multiplicationTable += '\n'
   }
+  multiplicationTable = multiplicationTable.substring(0, multiplicationTable.length-1)
+  return multiplicationTable
+}
+
+function GenerateCell(num1: number, num2: number):string{
+  return num1+ '*' + num2 + '=' + num1*num2
+}
+
+function IsInputVaild(start: number, end: number):boolean{
+  if(!IsStartNotGreaterThanEnd(start, end))
+  {
+    return false
+  }
+  if(!IsInputIntheRange(start, end))
+  {
+    return false
+  }
+  return true
+}
+
+function IsStartNotGreaterThanEnd(start: number, end: number):boolean{
+  if(start>end)
+  {
+    return false
+  }
+  return true
+}
+
+function IsInputIntheRange(start: number, end: number):boolean{
+  if(start<1 || start>10 || end<1 || end>10)
+  {
+    return false
+  }
+  return true
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -13,4 +13,17 @@ describe('Multiplication Table', () => {
     //then
     expect(rendered).toBe('1*1=1')
   })
+
+  it('should render multiplication table of (2, 2)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 2
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('2*2=4')
+  })
 })
