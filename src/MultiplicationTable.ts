2806875dc70bb536ca7baa75ae43f53526b32c22
RZhang16 <RZhang16@slb.com>
2022-10-25 17:21:35 +0800

    feat: Add new functionl

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -3,3 +3,22 @@ export class MultiplicationTable {
     return '1*1=1'
   }
 }
+
+function IsNumberValid(start: number, end: number): boolean{
+  return start <= end && start >= 1 && end <= 10
+}
+
+function GenerateMultipleResult(start: number, end: number): string{
+  var Result = ''
+  for(let i = start; i <= end; i++){
+    for(let z = start; z <= end; z++){
+      Result += `${i} * ${z} = ${i*z}`;
+    }
+  }
+  return Result
+}
+
+function GenerateMultipleTable(result: string): string{
+
+
+}
