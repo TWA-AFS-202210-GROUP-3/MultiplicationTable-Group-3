311375c95adfd1c213163c7540a25a9421658345
Bingqi <BLiu25@slb.com>
2022-10-25 21:50:29 +0800

    feat: add generateMultiplicationTable func

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,10 +1,12 @@
 export class MultiplicationTable {
-  public render(start: number, end: number): string{
+  public render(start: number, end: number): string {
     const startNotGreaterThanEnd = isStartNotGreaterThanEnd(start,end)
     const inTheRange = isInTheRange(start,end)
     if (startNotGreaterThanEnd == true && inTheRange == true)
     {
-      return '1*1=1'
+      
+      const generatedMultiplicationTable = generateMultiplicationTable(start,end)
+      return generatedMultiplicationTable
     }
     else {
       return 'null'}
@@ -17,5 +19,25 @@ export function isStartNotGreaterThanEnd(start:number,end:number):boolean{
 
 export function isInTheRange(start:number,end:number):boolean{
   return start>=1 && end<=10
+}
+
+export function generateMultiplicationTable(start:number,end:number):string {
+  let generatedMultiplicationExpressions: string = ''
+  let expressions: string = ''
+  for(let i = start; i <= end; i ++ ){
+    for(let j = start; j <= i; j ++){
+      if (start == end){
+        expressions = i + '*' + i + '=' + i * j
+      }
+      else{
+        expressions = i + '*' + i + '=' + i * j + '  '
+      }
+      generatedMultiplicationExpressions = generatedMultiplicationExpressions + expressions   
+    }
+    if (start != end){
+      generatedMultiplicationExpressions = generatedMultiplicationExpressions + '\n'
+    }
+  }
+  return generatedMultiplicationExpressions
 }
 
