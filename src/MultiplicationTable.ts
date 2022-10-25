761544763ada119e652abdfb98c783b993c37ca5
RZhang16 <RZhang16@slb.com>
2022-10-25 20:25:50 +0800

    fix: fix test and add jest coveraget

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -12,11 +12,11 @@ export function render(start: number, end: number): string{
   return Result
 }
 
-function IsNumberValid(start: number, end: number): boolean{
+export function IsNumberValid(start: number, end: number): boolean{
   return start <= end && start >= 1 && end <= 10
 }
 
-function GenerateMultipleResult(start: number, end: number): string{
+export function GenerateMultipleResult(start: number, end: number): string{
   var Result = ''
   for(let i = start; i <= end; i++){
       Result += `${i}*${end}=${i*end}`;
