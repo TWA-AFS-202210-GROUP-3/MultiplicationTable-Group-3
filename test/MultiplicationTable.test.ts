260b4b560d479e954e1f6a8fc981967a593df35d
Zejie Yao <ZYao3@slb.com>
2022-10-25 17:24:18 +0800

    feat: add new logic

--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -4,13 +4,18 @@ describe('Multiplication Table', () => {
   it('should render multiplication table of (1, 1)', () => {
     // given
     const table = new MultiplicationTable()
-    const start = 1
-    const end = 1
+    const start = 2
+    const end = 4
+    const resultArr: string[] = []
 
     //when
-    const rendered = table.render(start, end)
+    const rendered = table.render(start, end, resultArr)
+
+    const result = `2*2=4
+    2*3=6  3*3=9
+    2*4=8  3*4=12  4*4=16`
 
     //then
-    expect(rendered).toBe('1*1=1')
+    expect(rendered).toBe(result)
   })
 })
