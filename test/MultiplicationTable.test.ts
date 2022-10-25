e3e78d4d2a069537714a700536d0e995d8dbe887
RZhang16 <RZhang16@slb.com>
2022-10-25 20:23:42 +0800

    feat: Add new test

--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,4 +1,4 @@
-import {render} from '../src/MultiplicationTable'
+import {render, IsNumberValid} from '../src/MultiplicationTable'
 
 describe('Multiplication Table', () => {
   it('should render multiplication table of (1, 1)', () => {
@@ -20,6 +20,22 @@ describe('Multiplication Table', () => {
     const rendered = render(start, end)
 
     expect(rendered).toBe('2*2=4\n2*3=6  3*3=9\n2*4=8  3*4=12  4*4=16')
+  })
+
+  it('should be true', () => {
+    const start = 2
+    const end = 4
+    const validResult = IsNumberValid(start, end)
+
+    expect(validResult).toBe(true)
+  })
+
+  it('should be true', () => {
+    const start = 2
+    const end = 11
+    const validResult = IsNumberValid(start, end)
+
+    expect(validResult).toBe(false)
   })
 
 })
