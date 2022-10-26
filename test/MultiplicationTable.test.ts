cda0d62f27d130db0c598b93b60cb8b468558aa9
hdeng <hdeng10@slb.com>
2022-10-26 09:33:06 +0800

    'updated'

--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -51,4 +51,19 @@ describe('Multiplication Table', () => {
     //then
     expect(rendered).toBe('3*3=9')
   })
+  it('should render multiplication table of (2, 3)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 3
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    const expected =`2*2=4
+2*3=6  3*3=9`
+    expect(rendered).toBe(expected)
+  })
+
 })
